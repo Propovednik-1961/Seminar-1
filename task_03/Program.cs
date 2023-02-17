d0842d44c5027054cf9c9860e1cde4ddaf1edf69
Alexey Novichkov <alexey-mashine@yandex.ru>
2023-02-17 16:47:23 +0300

    Добавили файл task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.Write("понедельник ");
+}
+else if (day == 2)
+{
+    Console.Write("вторник ");
+}
+else if (day == 3)
+{
+    Console.Write("среда "); 
+}
+else if (day == 4)
+{
+    Console.Write("четверг" );
+}
+else if (day == 5)
+{
+    Console.Write("пятница ");
+}
+else if (day == 6)
+{
+    Console.Write("суббота " );
+}
+else if (day == 7)
+{
+    Console.Write("воскресенье ");
+}
+else
+{
+    Console.Write("Такого дня недели нет ");
+}
